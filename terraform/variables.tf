9bfbcc051faf284bd1c6ab4d4479b6b6ec391a0b
unkown007 <victormatusse00@gmail.com>
2025-08-19 01:44:30 +0200

    fix free teer ec2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "public_key_openssh" {
 variable "instance_type" {
   description = "EC2 instance type."
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "name_prefix" {
